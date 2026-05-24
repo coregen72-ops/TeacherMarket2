8872cd628358f4c5ff726881e0648239eeebd246
Rakshitha Khodanpur <rakshithakhodanpur@gmail.com>
2026-05-24 15:39:44 +0530

    updated project

--- finalproject/frontend/vite.config.js
+++ finalproject/frontend/vite.config.js
@@ -1,4 +1,3 @@
-
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -7,6 +6,10 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     server: {
+      headers: {
+        "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+        "Cross-Origin-Embedder-Policy": "unsafe-none"
+      },
       proxy: !env.VITE_API_URL
         ? {
             '/auth': { target: 'http://localhost:5000', changeOrigin: true },
